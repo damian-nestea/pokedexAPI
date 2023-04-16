4c8379580a31e2b4ed0a6db9d379517a864576dd
damian-nestea <damian@hotmail.cz>
2023-04-16 20:03:22 -0300

    feature estilização básica do componente header, adição de logo, criação de botões Home e Pokedex

--- react-pokedex/src/Components/Header/headerStyle.js
+++ react-pokedex/src/Components/Header/headerStyle.js
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  background-color: green;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  background-color: #FFF;
+  height:10rem;
+  padding: 2rem 4rem;
+`
+
+export const PokemonLogo = styled.img`
+  width: 15rem;
+`
+
+export const HomeButton = styled.button`
+  background-color: #fff;
+  padding:0;
+  font-size:1.1rem;
+  font-weight:bold;
+  text-decoration:underline;
+  border:none;
+  cursor: pointer;
+`
+
+export const PokedexButton = styled.button`
+  background-color: #33A4F5;
+  color: #fff;
+  padding: 1rem 2.5rem;
+  font-size:1.1rem;
+  font-weight:bold;
+  border:none;
+  border-radius:0.25rem;
+  cursor: pointer;
 `
