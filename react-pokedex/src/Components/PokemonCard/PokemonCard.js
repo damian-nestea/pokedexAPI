38e76e69d748299480dd253fbb93ded54c3b6255
damian-nestea <damian@hotmail.cz>
2023-04-22 17:52:51 -0300

    feature estilização do card de pokemon com flex, alinhamento de imagem e info do card

--- react-pokedex/src/Components/PokemonCard/PokemonCard.js
+++ react-pokedex/src/Components/PokemonCard/PokemonCard.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import { CaptureButton, PokemonCardContainer, DetailsLink, Image, Name, MainInfo } from './pokemonCardStyle'
+import React from "react";
+import {
+  CaptureButton,
+  PokemonCardContainer,
+  DetailsLink,
+  Image,
+  Name,
+  MainInfo,
+  TopInfo,
+  BottomInfo,
+} from "./pokemonCardStyle";
 
 const PokemonCard = () => {
   return (
     <PokemonCardContainer>
-      <MainInfo>
-        <p>#01</p>
-        <Name>Bulbasaur</Name>
-      </MainInfo>
-      <Image src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' />
-      <DetailsLink href='/'>Detalhes</DetailsLink>
-      <CaptureButton>Capturar</CaptureButton>
+      <TopInfo>
+        <MainInfo>
+          <p>#01</p>
+          <Name>Bulbasaur</Name>
+        </MainInfo>
+        <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      </TopInfo>
+      <BottomInfo>
+        <DetailsLink href="/">Detalhes</DetailsLink>
+        <CaptureButton>Capturar</CaptureButton>
+      </BottomInfo>
     </PokemonCardContainer>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
--- react-pokedex/src/Components/PokemonCard/pokemonCardStyle.js
+++ react-pokedex/src/Components/PokemonCard/pokemonCardStyle.js
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 
 export const PokemonCardContainer = styled.article`
+  position:relative;
   background-color: lightblue;
   display:flex;
   flex-direction:column;
-  width: 15rem;
+  justify-content:space-between;
+  width: 20rem;
+  height: 10rem;
+  padding: 1rem;
+  border-radius:1rem;
 `
 export const Name = styled.h2`
   color: #fff;
 `
 export const Image = styled.img`
-  width: 12rem;
+  width: 8rem;
+  position:absolute;
+  top:-2rem;
+  right:1rem;
 `
 export const DetailsLink = styled.a`
 `
@@ -19,4 +27,19 @@ export const CaptureButton = styled.a`
   border-radius: 0.5rem;
   padding: 1rem 2rem;
 `
-export const MainInfo = styled.div``
+export const MainInfo = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:flex-start;
+  gap:0.2rem;
+`
+export const TopInfo = styled.div`
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+`
+export const BottomInfo = styled.div`
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+`
