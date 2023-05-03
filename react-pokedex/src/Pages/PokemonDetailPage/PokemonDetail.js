ce3414faad9e3151ce29981e55685ed5fade7d6a
damian-nestea <damian@hotmail.cz>
2023-05-03 20:10:11 -0300

    feature estilização do left-side do card de detalhes do pokemon com apresentação de imagens do pokemon pela frente e por trás

--- react-pokedex/src/Pages/PokemonDetailPage/PokemonDetail.js
+++ react-pokedex/src/Pages/PokemonDetailPage/PokemonDetail.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import { DetailsContainer, TituloDetails } from './pokemonDetailStyle'
-import Header from '../../Components/Header/Header'
+import React from "react";
+import {
+  DetailsBackground,
+  TituloDetails,
+  DetailsContainer,
+  PokemonBackAndFrontContainer,
+  PokemonBackFrontPhoto,
+  PokemonImage,
+} from "./pokemonDetailStyle";
+import Header from "../../Components/Header/Header";
 
 const PokemonDetail = () => {
   return (
     <>
-      <Header pagina={'details'}/>
-      <DetailsContainer>
+      <Header pagina={"details"} />
+      <DetailsBackground>
         <TituloDetails>Detalhes</TituloDetails>
-      </DetailsContainer>
+        <DetailsContainer>
+          <PokemonBackAndFrontContainer>
+            <PokemonBackFrontPhoto>
+              <PokemonImage src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            </PokemonBackFrontPhoto>
+            <PokemonBackFrontPhoto>
+              <PokemonImage src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            </PokemonBackFrontPhoto>
+          </PokemonBackAndFrontContainer>
+        </DetailsContainer>
+      </DetailsBackground>
     </>
-  )
-}
+  );
+};
 
-export default PokemonDetail
+export default PokemonDetail;
--- react-pokedex/src/Pages/PokemonDetailPage/pokemonDetailStyle.js
+++ react-pokedex/src/Pages/PokemonDetailPage/pokemonDetailStyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const DetailsContainer = styled.section`
+export const DetailsBackground = styled.div`
   background-color: #5e5e5e;
   padding: 2rem;
 `
@@ -10,4 +10,27 @@ export const TituloDetails = styled.h1`
   color: #fff;
   width:100%;
   margin-bottom:2rem;
+`
+export const DetailsContainer = styled.section`
+  background-color: #729f92;
+  padding: 2rem 3rem;
+  border-radius:1rem;
+`
+export const PokemonBackAndFrontContainer = styled.div`
+  width:18rem;
+  display:flex;
+  flex-direction:column;
+  gap:3rem;
+`
+export const PokemonBackFrontPhoto = styled.div`
+  background-color:#fff;
+  width:100%;
+  height:18rem;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  border-radius:0.5rem;
+`
+export const PokemonImage = styled.img`
+  width:40%;
 `
