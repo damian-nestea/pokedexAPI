bb77e7bca70e0cf6331c613fca6eba9f548ced70
damian-nestea <damian@hotmail.cz>
2023-05-06 19:00:27 -0300

    feature adição de BASE_URL para consumo de API de pokemon, criação de requisição via axios e feito no inicio da app com useEffect

--- react-pokedex/src/Pages/PokemonListPage/PokemonList.js
+++ react-pokedex/src/Pages/PokemonListPage/PokemonList.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { ListContainer, TituloPokemonList } from "./pokemonListStyle";
 import PokemonCard from "../../Components/PokemonCard/PokemonCard";
 import Header from "../../Components/Header/Header";
+import axios from "axios";
+import {BASE_URL} from "../../constants/url";
 
 const PokemonList = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    getAllPokemons();
+  },[])
+
+  const getAllPokemons = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}`);
+      console.log(res);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <>
-      <Header pagina={'pokelist'} />
+      <Header pagina={"pokelist"} />
       <ListContainer>
         <TituloPokemonList>Todos Pokémons</TituloPokemonList>
         <PokemonCard />
--- react-pokedex/src/contexts/GlobalState.js
+++ react-pokedex/src/contexts/GlobalState.js
@@ -2,6 +2,7 @@ import React from "react";
 import { GlobalContext } from "./GlobalContext";
 
 const GlobalState = ({ children }) => {
+  
   const data = {};
   return (
     <GlobalContext.Provider value={data}>{children}</GlobalContext.Provider>
