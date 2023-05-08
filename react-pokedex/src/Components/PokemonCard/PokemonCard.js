a9195e140c80521acd8fccb900b9d2147ea8e393
damian-nestea <damian@hotmail.cz>
2023-05-08 17:57:37 -0300

    feature requisição api para obter dados individuais de cada pokemon para colocar no card de apresentação da pokelist

--- react-pokedex/src/Components/PokemonCard/PokemonCard.js
+++ react-pokedex/src/Components/PokemonCard/PokemonCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CaptureButton,
   PokemonCardContainer,
@@ -13,15 +13,32 @@ import {
 } from "./pokemonCardStyle";
 import { useNavigate } from "react-router-dom";
 import { GoToDetailsPage } from "../../Router/coordinator";
+import axios from "axios";
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemonUrl }) => {
   const navigate = useNavigate();
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await axios.get(`${pokemonUrl}`);
+      console.log(res.data);
+      setPokemon(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <PokemonCardContainer>
       <TopInfo>
         <MainInfo>
           <IdPokemon>#01</IdPokemon>
-          <Name>Bulbasaur</Name>
+          <Name>{pokemon.name}</Name>
           <Types>Grass</Types>
         </MainInfo>
         <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
