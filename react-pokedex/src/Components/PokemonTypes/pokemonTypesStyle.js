fb9f91bbe1d46b1ef090607d4e9c2e905cae565b
damian-nestea <damian@hotmail.cz>
2023-05-19 18:59:39 -0300

    feat adição de componente Types na detailsPage com renderização condicional para aumento de tamanho comparado com o pokemonCard

--- react-pokedex/src/Components/PokemonTypes/pokemonTypesStyle.js
+++ react-pokedex/src/Components/PokemonTypes/pokemonTypesStyle.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const TypeContainer = styled.div`
-`
+  height: 1.6rem;
+  ${({ page }) => page === "detailsPage" && "height: 2.2rem;"}
+`;
 
 export const TypeImage = styled.img`
-  height:1.6rem;
-  margin-right:0.2rem;
-`
+  height: 100%;
+  margin-right: 0.2rem;
+`;
